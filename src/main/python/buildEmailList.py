0888fe36c1aff2622fc05099e2ab0d3da560a810
Jim Waldo <jiniwaldo@gmail.com>
2013-11-14 11:12:20 -0500

    Added buildEmailList.py, which will create a .csv with First Name, Last Name, email address on each line. This is more for the courses than for the researchers.

--- src/main/python/buildEmailList.py
+++ src/main/python/buildEmailList.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 '''
+Builds a csv file of First Name, Last Name, email address for a course
+
+This file needs to be run from a directory in which the users.csv and profiles.csv files
+exist. When run, it produces a mailAddresses.csv file with the above format. It will only
+include entries for students that are in the user.csv file. If there is a student in the
+users.csv file but not in the profiles.csv file, the student's name will read "Missing
+ Profile"
+
 Created on Nov 13, 2013
 
 @author: waldo
@@ -25,6 +33,9 @@ if __name__ == '__main__':
     outfile = csv.writer(open('mailAddresses.csv','w'))
     
     for uid in iter(udict):
-        name = pdict[uid].name
+        if uid in pdict:
+            name = pdict[uid].name
+        else :
+            name = 'Missing Profile'
         [first, last] = split(name)
         outfile.writerow([first, last, udict[uid].email])
