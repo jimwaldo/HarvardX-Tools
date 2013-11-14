210ac6bb50a834e5e9ea94bb420ed3712467638c
Jim Waldo <jiniwaldo@gmail.com>
2013-11-13 21:02:45 -0500

    Added a script to build a csv of firstName, lastName, emailAddress for a class

---
+++ src/main/python/buildEmailList.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''
+Created on Nov 13, 2013
+
+@author: waldo
+'''
+import demographics.userprofile as userp
+import user
+import sys
+import csv
+
+def split(name):
+    spIn = name.rfind(' ')
+    first = name[ :spIn]
+    last = name[spIn + 1: ]
+    return [first, last]
+
+if __name__ == '__main__':
+    ufile = csv.reader(open('users.csv','r'))
+    udict = user.builddict(ufile)
+    pfile = csv.reader(open('profiles.csv', 'r'))
+    pdict = userp.builddict(pfile)
+    
+    outfile = csv.writer(open('mailAddresses.csv','w'))
+    
+    for uid in iter(udict):
+        name = pdict[uid].name
+        [first, last] = split(name)
+        outfile.writerow([first, last, udict[uid].email])
