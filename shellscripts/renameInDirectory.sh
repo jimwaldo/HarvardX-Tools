f939f737a9ff26a7e8bf941d6beed0a4123257fb
Jim Waldo <jiniwaldo@gmail.com>
2014-04-07 11:30:06 -0400

    Removed a number of unused shell scripts (they have been replaced by python programs with more functionality)
    Removed two python scripts that have been superseeded by others with
    more functionality; these were quick hacks until we had a more
    principled way to do things.

--- shellscripts/renameInDirectory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/tcsh
-
-mv *auth_user.sql users.sql
-mv *userprofile.sql profiles.sql
-mv *certificate.sql certificates.sql
-mv *studentmodule.sql studentmodule.sql
-mv *studentmodulehistory.sql studentmodulehistory.sql
-mv *enrollment.sql enrollment.sql
-mv *.mongo forum.mongo
