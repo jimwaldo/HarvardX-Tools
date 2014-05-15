969c9ea1a65955076b5925668b452beaa411256c
Jim Waldo <jiniwaldo@gmail.com>
2014-05-15 11:45:32 -0400

    Additional documentation

--- src/main/python/logs/moveRawLogs.py
+++ src/main/python/logs/moveRawLogs.py
@@ -3,7 +3,8 @@
 Created on Feb 23, 2014
 
 Move the raw log files from their position under the various weekly directories to 
-a full directory of all of the raw log files. 
+a full directory of all of the raw log files. This is run to get the raw logs in the
+right place for the scripts that add them to the mongodb to process.
 '''
 import shutil
 import glob
