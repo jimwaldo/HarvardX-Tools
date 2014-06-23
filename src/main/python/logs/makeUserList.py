072db6c9f256a535f4c52dccea5cddd45f155787
Jim Waldo <jiniwaldo@gmail.com>
2014-06-23 07:26:28 -0400

    More cleanup

--- src/main/python/logs/makeUserList.py
+++ src/main/python/logs/makeUserList.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
+"""
+Extract the usernames from a JSON file, and write a file of those usernames.
 
+Given a file with a field marked 'username', extract the values of those fields
+and write, one per line, to a file with the supplied name.
+"""
 import json
 import sys
 
-f1 = open(sys.argv[1], 'r')
-f2 = open(sys.argv[2], 'w')
-dc = json.JSONDecoder()
 
-for line in f1:
-    dcl = dc.decode(line)
-    f2.write(dcl['username'] + '\n')
+if __name__ == '__main__':
+    f1 = open(sys.argv[1], 'r')
+    f2 = open(sys.argv[2], 'w')
+    dc = json.JSONDecoder()
+    for line in f1:
+        dcl = dc.decode(line)
+        f2.write(dcl['username'] + '\n')
+
+
