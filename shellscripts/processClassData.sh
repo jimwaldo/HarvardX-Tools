eb957b81373d9f7beac87d2bf2a9f3fb6fe8e23d
SdotGlenn <glenn_lopez@harvard.edu>
2017-01-24 09:09:07 -0500

    Create new HarvardXPLUS directory in processClassData and rename edx email file list

--- shellscripts/processClassData.sh
+++ shellscripts/processClassData.sh
@@ -4,19 +4,33 @@
 killListedFiles.py
 #Next, rename filenames for course that are common between HarvardX/MITx, since org id is not specified
 renameCommonCourses.py
+
+# HarvardX
 mkdir HarvardX
 mv HarvardX-* HarvardX
+
+# Harvard
 mkdir Harvard
 mv Harvard-* Harvard/
 mv HARVARD-* Harvard/
+
+# HSPH
 mkdir HSPH
 mv HSPH-* HSPH/
+
+# HarvardKennedy School
 mkdir HarvardKennedySchool
 mv HarvardKennedySchool-* HarvardKennedySchool/
 
+# HarvardXPlus
+mkdir HarvardXPLUS
+mv HarvardXPLUS-* HarvardXPLUS/
+
+
 # Move Weekly email optin list
 mkdir HarvardX/CombinedCourseData
-mv harvardx-email_opt_in-* HarvardX/CombinedCourseData
+#mv harvardx-email_opt_in-* HarvardX/CombinedCourseData # Prior to 2016-09-05, this was the naming convetion
+mv HarvardX/HarvardX-email_opt_in-* HarvardX/CombinedCourseData # New name, as of 2016-09-05
 
 #Now, decrypt everything, set up separate directories for each class, move files
 #to those directories, and rename to something shorter and more uniform, and then
